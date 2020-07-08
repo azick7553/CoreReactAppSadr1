ffc1b03176d34512839f4ea5ad685552dbe066f8
azick7553 <azick7553@gmail.com>
2020-07-08 19:45:38 +0500

    Adding API

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API.Models;
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class ValuesController : ControllerBase
+    {
+        public static List<Value> ValuesList = new List<Value>()
+        {
+            new Value{ Id = Guid.NewGuid(), LastName = "Bob1", FirstName = "BobName1" },
+            new Value{ Id = Guid.NewGuid(), LastName = "Bob2", FirstName = "BobName2" },
+            new Value{ Id = Guid.NewGuid(), LastName = "Bob3", FirstName = "BobName3" },
+            new Value{ Id = Guid.NewGuid(), LastName = "Bob4", FirstName = "BobName4" },
+            new Value{ Id = Guid.NewGuid(), LastName = "Bob5", FirstName = "BobName5" }
+        };
+
+        [HttpGet]
+        [Route("GetValues")]
+        public IActionResult GetValues()
+        {
+            return new JsonResult(ValuesList);
+        }
+
+    }
+}
---
+++ API/Models/Value.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API.Models
+{
+    public class Value
+    {
+        public Guid Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
